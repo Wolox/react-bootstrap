30e5c5fb56ac42defa227b3f1e3596e73ca68732
damfinkel <dfinkelstein@wolox.com.ar>
2018-07-04 20:13:44 -0300

    moved all configuration from run.sh to yeoman

---
+++ generators/app/tasks/configPackageJson.js
@@ -0,0 +1,38 @@
+const generateRARScript = (command, options) => `react-app-rewired ${command} --scripts-version wolox-react-scripts ${options}`;
+
+const getPackageJsonAttributes = (
+  projectName,
+  projectVersion,
+  repoUrl
+) => 
+({
+  name: projectName,
+  title: projectName,
+  version: projectVersion,
+  repository: {
+    type: "git",
+    url: repoUrl
+  },
+  scripts: {
+    start: generateRARScript("start"),
+    build: generateRARScript("build"),
+    test: generateRARScript("test", "--env=jsdom"),
+    eject: "./node_modules/react-scripts/bin/react-scripts.js eject",
+    deploy: generateRARScript("build"),
+    lint: "./node_modules/eslint/bin/eslint.js src",
+    "lint-fix": "./node_modules/eslint/bin/eslint.js src --fix",
+    "lint-diff":
+      "git diff --name-only --cached --relative | grep \\.js$ | xargs eslint",
+    precommit: "npm run lint-diff"
+  } 
+});
+
+module.exports = function configPackageJson() {
+  const pjson = this.fs.readJSON(`./${this.projectName}/package.json`);
+  const newpjson = Object.assign(pjson, getPackageJsonAttributes(
+    this.projectName,
+    "1.0.0",
+    this.repoUrl
+  ));
+  this.fs.writeJSON(`./${this.projectName}/package.json`, newpjson);
+};
---
+++ generators/app/tasks/utils.js
@@ -0,0 +1,15 @@
+
+module.exports.copy = function copy(src, des) {
+  this.fs.copy(
+    this.templatePath(src),
+    this.destinationPath(`${this.projectName}/${des}`)
+  );
+};
+
+module.exports.copyTpl = function copyTpl(src, des, temp) {
+  this.fs.copyTpl(
+    this.templatePath(src),
+    this.destinationPath(`${this.projectName}/${des}`),
+    temp
+  );
+};
