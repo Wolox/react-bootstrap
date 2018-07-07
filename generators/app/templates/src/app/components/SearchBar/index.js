a1b90d12d0f6fa3f8425661c4409f1d2836c6625
Nicolás <nicolas.lobruno@wolox.com.ar>
2018-07-07 12:55:27 -0300

    corrections button

--- generators/app/templates/src/app/components/SearchBar/index.js
+++ generators/app/templates/src/app/components/SearchBar/index.js
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 
 import SearchBar from './layout';
 
-function SearchBarContainer({ stylesSearchButton, textButtonSearch, className, initialValues, getElements }) {
+function SearchBarContainer({ textButtonSearch, className, initialValues, getElements }) {
   const handleSubmit = formValues => getElements(formValues);
   return (
     <SearchBar
       onSubmit={handleSubmit}
-      stylesSearchButton={stylesSearchButton}
       textButtonSearch={textButtonSearch}
       className={className}
       initialValues={initialValues}
@@ -19,16 +18,10 @@ function SearchBarContainer({ stylesSearchButton, textButtonSearch, className, i
 }
 
 SearchBarContainer.propTypes = {
-  initialValues: PropTypes.object, // eslint-disable-line
+  initialValues: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   getElements: PropTypes.func.isRequired,
   textButtonSearch: PropTypes.string,
-  className: PropTypes.string,
-  stylesSearchButton: PropTypes.string
-};
-
-SearchBarContainer.defaultProps = {
-  className: '',
-  textButtonSearch: 'Buscar'
+  className: PropTypes.string
 };
 
 export default SearchBarContainer;
--- generators/app/templates/src/app/components/SearchBar/layout.js
+++ generators/app/templates/src/app/components/SearchBar/layout.js
@@ -2,30 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm } from 'redux-form';
 
-import Button from '../Button';
-
 import { SEARCH_FORM } from './constants';
 
-function SearchBar({ stylesSearchButton, textButtonSearch, className, children }) {
+function SearchBar({ textButtonSearch, className, children }) {
   return (
     <div className={className}>
       {children}
-      <Button.button type="submit" className={stylesSearchButton}>
+      <button type="submit">
         {textButtonSearch}
-      </Button.button>
+      </button>
     </div>
   );
 }
 
 SearchBar.propTypes = {
   textButtonSearch: PropTypes.string,
-  className: PropTypes.string,
-  stylesSearchButton: PropTypes.string
+  className: PropTypes.string
 };
 
 SearchBar.defaultProps = {
   className: '',
-  stylesSearchButton: ''
+  textButtonSearch: 'Buscar'
 };
 
 export default reduxForm({
