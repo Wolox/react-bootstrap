043ab8cc150fa977cfe3d5dfb547d26e0a95c576
damfinkel <dfinkelstein@wolox.com.ar>
2018-08-16 17:48:06 -0300

    added sass modules to storybook webpack config

--- generators/app/constants.js
+++ generators/app/constants.js
@@ -1,63 +1,65 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
 
-module.exports.KICKOFF_MESSAGE = `${chalk.cyan('\n        ║║║            ║║║       ') +
-  chalk.cyan('\n       ║    ║║║    ║║║    ║      ') +
-  chalk.cyan('\n       ║       ║║║║       ║       ') +
-  chalk.cyan('\n        ║    ║║ ║║ ║║    ║           ') +
-  chalk.cyan('\n        ║║║ ║║║║  ║║║║ ║║║        ') +
-  chalk.cyan('\n     ║║║ ║ ║          ║ ║ ║║║          ') +
-  chalk.blue('██╗  ██╗██╗ ██████╗██╗  ██╗ ') +
-  chalk.white('██████╗ ███████╗███████╗') +
-  chalk.cyan('\n   ║║    ║║║   ║║║║   ║║║    ║║        ') +
-  chalk.blue('██║ ██╔╝██║██╔════╝██║ ██╔╝') +
-  chalk.white('██╔═══██╗██╔════╝██╔════╝') +
-  chalk.cyan('\n  ║       ║   ║║║║║║   ║       ║       ') +
-  chalk.blue('█████╔╝ ██║██║     █████╔╝ ') +
-  chalk.white('██║   ██║█████╗  █████╗') +
-  chalk.cyan('\n   ║║    ║║║   ║║║║   ║║║    ║║        ') +
-  chalk.blue('██╔═██╗ ██║██║     ██╔═██╗ ') +
-  chalk.white('██║   ██║██╔══╝  ██╔══╝') +
-  chalk.cyan('\n     ║║║ ║ ║          ║ ║ ║║║          ') +
-  chalk.blue('██║  ██╗██║╚██████╗██║  ██╗') +
-  chalk.white('╚██████╔╝██║     ██║') +
-  chalk.cyan('\n        ║║║ ║║║║  ║║║║ ║║║             ') +
-  chalk.blue('╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝ ') +
-  chalk.white('╚═════╝ ╚═╝     ╚═╝') +
-  chalk.cyan('\n        ║    ║║ ║║ ║║    ║         ') +
-  chalk.cyan('\n       ║       ║║║║       ║          ') +
-  chalk.cyan('\n       ║    ║║║    ║║║    ║        ') +
-  chalk.cyan('\n        ║║║            ║║║      ')}\n\n  ${chalk.blue.bold(
-  'Welcome to the React kickoff'
+module.exports.KICKOFF_MESSAGE = `${chalk.cyan(
+  "\n        ║║║            ║║║       "
+) +
+  chalk.cyan("\n       ║    ║║║    ║║║    ║      ") +
+  chalk.cyan("\n       ║       ║║║║       ║       ") +
+  chalk.cyan("\n        ║    ║║ ║║ ║║    ║           ") +
+  chalk.cyan("\n        ║║║ ║║║║  ║║║║ ║║║        ") +
+  chalk.cyan("\n     ║║║ ║ ║          ║ ║ ║║║          ") +
+  chalk.blue("██╗  ██╗██╗ ██████╗██╗  ██╗ ") +
+  chalk.white("██████╗ ███████╗███████╗") +
+  chalk.cyan("\n   ║║    ║║║   ║║║║   ║║║    ║║        ") +
+  chalk.blue("██║ ██╔╝██║██╔════╝██║ ██╔╝") +
+  chalk.white("██╔═══██╗██╔════╝██╔════╝") +
+  chalk.cyan("\n  ║       ║   ║║║║║║   ║       ║       ") +
+  chalk.blue("█████╔╝ ██║██║     █████╔╝ ") +
+  chalk.white("██║   ██║█████╗  █████╗") +
+  chalk.cyan("\n   ║║    ║║║   ║║║║   ║║║    ║║        ") +
+  chalk.blue("██╔═██╗ ██║██║     ██╔═██╗ ") +
+  chalk.white("██║   ██║██╔══╝  ██╔══╝") +
+  chalk.cyan("\n     ║║║ ║ ║          ║ ║ ║║║          ") +
+  chalk.blue("██║  ██╗██║╚██████╗██║  ██╗") +
+  chalk.white("╚██████╔╝██║     ██║") +
+  chalk.cyan("\n        ║║║ ║║║║  ║║║║ ║║║             ") +
+  chalk.blue("╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝ ") +
+  chalk.white("╚═════╝ ╚═╝     ╚═╝") +
+  chalk.cyan("\n        ║    ║║ ║║ ║║    ║         ") +
+  chalk.cyan("\n       ║       ║║║║       ║          ") +
+  chalk.cyan("\n       ║    ║║║    ║║║    ║        ") +
+  chalk.cyan("\n        ║║║            ║║║      ")}\n\n  ${chalk.blue.bold(
+  "Welcome to the React kickoff"
 )}\n\n  `;
 
 module.exports.LINTER_PATH = {
-  src: 'eslintrc.js',
-  destination: '.eslintrc.js'
+  src: "eslintrc.js",
+  destination: ".eslintrc.js"
 };
 
 module.exports.FLOWCONFIG_PATH = {
-  src: 'flowconfig',
-  destination: '.flowconfig'
+  src: "flowconfig",
+  destination: ".flowconfig"
 };
 
-const COMPONENTS_PATH = 'src/app/components';
-const SCREENS_PATH = 'src/app/screens';
-const CONFIG_PATH = 'src/config';
-const REDUX_PATH = 'src/redux';
-const UTILS_PATH = 'src/utils';
-const SERVICES_PATH = 'src/services';
-const CONSTANTS_PATH = 'src/constants';
-const STORYBOOK_CONFIG_PATH = '.storybook/';
-const STORIES_PATH = 'stories/';
+const COMPONENTS_PATH = "src/app/components";
+const SCREENS_PATH = "src/app/screens";
+const CONFIG_PATH = "src/config";
+const REDUX_PATH = "src/redux";
+const UTILS_PATH = "src/utils";
+const SERVICES_PATH = "src/services";
+const CONSTANTS_PATH = "src/constants";
+const STORYBOOK_CONFIG_PATH = ".storybook/";
+const STORIES_PATH = "stories/";
 
 module.exports.REDUX_COMPONENTS = [`${COMPONENTS_PATH}/Field/index.js`];
 
 module.exports.TEMPLATE_FILES = [
-  'config-overrides.js',
-  'pull_request_template.md',
-  'circle.yml',
-  'src/index.js',
-  'src/app/index.js',
+  "config-overrides.js",
+  "pull_request_template.md",
+  "circle.yml",
+  "src/index.js",
+  "src/app/index.js",
   `${CONFIG_PATH}/api.js`,
   `${CONFIG_PATH}/i18n.js`,
   `${CONFIG_PATH}/numeral.js`,
@@ -84,7 +86,9 @@ module.exports.TEMPLATE_FILES = [
   `${CONSTANTS_PATH}/fonts.js`,
   `${CONSTANTS_PATH}/sizes.js`,
   `${STORYBOOK_CONFIG_PATH}/config.js`,
-  `${STORIES_PATH}/components.js`
+  `${STORYBOOK_CONFIG_PATH}/webpack.config.js`,
+  `${STORIES_PATH}/components.js`,
+  `${STORIES_PATH}/styles.scss`
 ];
 
 module.exports.LOCAL_STORAGE_FILE = `${SERVICES_PATH}/LocalStorageService.js`;
@@ -92,27 +96,27 @@ module.exports.LOCAL_STORAGE_FILE = `${SERVICES_PATH}/LocalStorageService.js`;
 module.exports.OPTIONAL_DEPENDENCIES = {
   redux: {
     dependencies: [
-      'redux',
-      'redux-recompose',
-      'redux-form',
-      'redux-thunk',
-      'react-router',
-      'react-router-redux',
-      'redux-beacon'
+      "redux",
+      "redux-recompose",
+      "redux-form",
+      "redux-thunk",
+      "react-router",
+      "react-router-redux",
+      "redux-beacon"
     ]
   },
-  apisauce: { dependencies: ['apisauce'] },
-  'babel-module-resolver': {
-    dependencies: ['babel-plugin-module-resolver'],
-    devDependencies: ['eslint-import-resolver-babel-module']
+  apisauce: { dependencies: ["apisauce"] },
+  "babel-module-resolver": {
+    dependencies: ["babel-plugin-module-resolver"],
+    devDependencies: ["eslint-import-resolver-babel-module"]
   },
-  jest: { dependencies: ['jest'] },
-  moment: { dependencies: ['moment'] },
-  'prop-types': { dependencies: ['prop-types'] },
-  'seamless-immutable': { dependencies: ['seamless-immutable'] },
+  jest: { dependencies: ["jest"] },
+  moment: { dependencies: ["moment"] },
+  "prop-types": { dependencies: ["prop-types"] },
+  "seamless-immutable": { dependencies: ["seamless-immutable"] },
   flow: {
-    dependencies: ['babel-cli'],
-    devDependencies: ['flow-bin', 'babel-preset-flow']
+    dependencies: ["babel-cli"],
+    devDependencies: ["flow-bin", "babel-preset-flow"]
   },
-  reselect: { dependencies: ['reselect'] }
+  reselect: { dependencies: ["reselect"] }
 };
--- generators/app/templates/stories/components.js
+++ generators/app/templates/stories/components.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
-import SearchBar from '../src/app/components/SearchBar';
-import TextArea from '../src/app/components/TextArea';
+import SearchBar from "../src/app/components/SearchBar";
+import TextArea from "../src/app/components/TextArea";
 
-storiesOf('SearchBar', module).add('Default', () => (
-  <SearchBar textButtonSearch="Search" handleSubmit={action('clicked')}>
+import styles from "./styles.scss";
+
+storiesOf("SearchBar", module).add("Default", () => (
+  <SearchBar
+    textButtonSearch="Search"
+    className={styles.searchBarContainer}
+    handleSubmit={action("clicked")}
+  >
     <input name="input1" />
     <input name="input2" />
   </SearchBar>
 ));
 
-storiesOf('TextArea', module).add('Default', () => <TextArea onChange={action('changed')} />);
+storiesOf("TextArea", module).add("Default", () => (
+  <TextArea onChange={action("changed")} />
+));
