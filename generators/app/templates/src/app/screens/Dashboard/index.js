a4bc35e581f48ecb9731cea0c36c7a843d98e9df
damfinkel <damfinkel@gmail.com>
2018-11-13 17:38:48 -0300

    fixed some linter issues and added jsonconfig.json file

--- generators/app/templates/src/app/screens/Dashboard/index.js
+++ generators/app/templates/src/app/screens/Dashboard/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Routes from '../../../constants/routes';
+
 import Home from './screens/Home';
 
 function Dashboard() {
