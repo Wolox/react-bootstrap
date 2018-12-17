0614db05d40bc90a81feab0ee982d8d8f07b6f3d
Enrique Mejia <enrique.mejia@wolox.co>
2018-12-17 14:40:57 -0500

    Edited script to use curl for downloading the npm-no-sudo script. Also allowed verbose information to be passed onto the command to be executed

--- run.sh
+++ run.sh
@@ -2,14 +2,6 @@
 
 { # this ensures the entire script is downloaded #
 
-printf "\n\nThis script requires an npm configuration that allows global\n"
-printf "installs without sudo. We'll be checking whether you have globally\n"
-printf "installed packages and if so, we'll be moving them to a new folder.\n\n"
-printf "You may have to type in your password once to allow moving all\n"
-printf "your currently installed packages.\n\n"
-
-./scripts/npm-no-sudo.sh
-
 echo "Getting initial dependencies..."
 
 system_has() {
@@ -25,15 +17,33 @@ if ! system_has git; then
   exit 1
 
 elif [ "$(printf '%s\n' "$requirednodeversion" "$currentnodeversion" | sort -V | head -n1)" != "$requirednodeversion" ]; then 
-  echo "The node version must be >= v6.2.0"  
+  echo "The node version must be >= v6.2.0"
   exit 1
 fi
 
+
+printf "\n\nThis script requires an npm configuration that allows global\n"
+printf "installs without sudo. We'll be checking whether you have globally\n"
+printf "installed packages and if so, we'll be moving them to a new folder.\n\n"
+printf "You may have to type in your password once to allow moving all\n"
+printf "your currently installed packages.\n\n"
+
 if [[ $1 == '--verbose' || $1 == '-v' ]]
+then if [[ $1 == '--local' || $1 == '-l']]
 then
-  npm i -g yo generator-react-bootstrap
+  ./scripts/npm-no-sudo.sh
 else
-  npm i -g yo generator-react-bootstrap > /dev/null 2>&1
+  bash <(curl -s https://raw.githubusercontent.com/Wolox/react-bootstrap/development/scripts/npm-no-sudo.sh) [--verbose]
+fi
+npm i -g yo generator-react-bootstrap
+else
+if [[ $1 == '--local' || $1 == '-l']]
+then
+  ./scripts/npm-no-sudo.sh
+else
+  bash <(curl -s https://raw.githubusercontent.com/Wolox/react-bootstrap/development/scripts/npm-no-sudo.sh)
+fi
+npm i -g yo generator-react-bootstrap > /dev/null 2>&1
 fi
 
 if [[ $1 == '--local' || $1 == '-l' ]]
