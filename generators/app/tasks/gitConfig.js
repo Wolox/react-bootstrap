1c97b07801473cf0f977cf9d7ba978f4492f8741
mona <franco.perez@wolox.com.ar>
2019-01-25 14:08:35 -0300

    add -f to git push in order to avoid future problems

--- generators/app/tasks/gitConfig.js
+++ generators/app/tasks/gitConfig.js
@@ -39,7 +39,7 @@ module.exports.configGit = function configGit() {
       runCommand({
         command: [
           'git',
-          ['push', 'origin', 'master:repository-initiation'],
+          ['push', '-f', 'origin', 'master:repository-initiation'],
           { cwd: `${process.cwd()}/${this.projectName}` }
         ],
         loadingMessage: 'Pushing all files to repository',
