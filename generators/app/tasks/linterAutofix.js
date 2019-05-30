4b6ea30ba0abe6d9a8d9b0938a393f58132299fe
carlos <carlosmbollero@gmail.com>
2019-05-30 11:41:51 -0300

    Add run linter autofix command after bootstraping project

---
+++ generators/app/tasks/linterAutofix.js
@@ -0,0 +1,10 @@
+const runCommand = require('./runCommand');
+
+module.exports = function linterAutofix() {
+  return runCommand({
+    command: ['npm', ['run', 'lint-fix'], { cwd: `${process.cwd()}/${this.projectName}` }],
+    loadingMessage: 'Running linter autofix',
+    successMessage: 'Linter autofix ready!',
+    failureMessage: 'Linter autofix failed!'
+  });
+};
