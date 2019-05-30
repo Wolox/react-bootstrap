647e41b07f34038b2fb1aff06b49fedfff59e085
mona <franco.perez@wolox.com.ar>
2019-05-30 13:05:19 -0300

    add no-duplicate-dollar-variable rule in order to avoid errors

--- generators/app/templates/.stylelintrc.js
+++ generators/app/templates/.stylelintrc.js
@@ -3,7 +3,10 @@ module.exports = {
   rules: {
     'selector-max-type': 2,
     'at-rule-no-unknown': [ true, {
-      ignoreAtRules: ['if', 'for', 'extend']
+      ignoreAtRules: ['if', 'for', 'extend', 'mixin']
+    }],
+    'scss/no-duplicate-dollar-variables': [ true, {
+      ignoreInsideAtRules: ['if', 'mixin', 'media']
     }]
   }
 };
