14306bb3fd971ef1946b54b13ef01190f335b12b
jjuli93 <alvarezjulianignacio@gmail.com>
2019-10-15 17:54:57 -0300

    changed i18next import

--- generators/app/templates/src/app/screens/Login/layout.tsx
+++ generators/app/templates/src/app/screens/Login/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { t } from 'i18next';
+import i18next from 'i18next';
 
 import InputLabel from '~components/InputLabel';
 import Routes from '~constants/routes';
@@ -17,36 +17,36 @@ function Login({ onEmailChange, onPasswordChange, onLogin }: Props) {
   return (
     <form className={`column center full-width m-top-8 ${styles.formContainer}`} onSubmit={onLogin}>
       <div className="column center m-bottom-3">
-        <h2 className="m-bottom-1">{t('Login:login')}</h2>
-        <h3>{t('Login:loginExplanation')}</h3>
+        <h2 className="m-bottom-1">{i18next.t('Login:login')}</h2>
+        <h3>{i18next.t('Login:loginExplanation')}</h3>
       </div>
       <div className={`column m-bottom-2 ${styles.sectionContainer}`}>
         <InputLabel
-          label={t('Login:email')}
+          label={i18next.t('Login:email')}
           name={FIELDS.email}
           inputId={FIELDS.email}
           dataFor={FIELDS.email}
           inputType="text"
           inputClassName={`m-bottom-2 full-width ${styles.input}`}
-          placeholder={t('Login:emailPlaceholder')}
+          placeholder={i18next.t('Login:emailPlaceholder')}
           handleChange={onEmailChange}
         />
         <InputLabel
-          label={t('Login:password')}
+          label={i18next.t('Login:password')}
           name={FIELDS.password}
           inputId={FIELDS.password}
           dataFor={FIELDS.password}
           inputType="password"
           inputClassName={`m-bottom-2 full-width ${styles.input}`}
-          placeholder={t('Login:passwordPlaceholder')}
+          placeholder={i18next.t('Login:passwordPlaceholder')}
           handleChange={onPasswordChange}
         />
       </div>
       <div className={`column center ${styles.sectionContainer}`}>
         <button type="submit" className={`full-width m-bottom-1 ${styles.button}`}>
-          {t('Login:enter')}
+          {i18next.t('Login:enter')}
         </button>
-        <a href={Routes.RECOVER_PASSWORD}>{t('Login:forgotPassword')}</a>
+        <a href={Routes.RECOVER_PASSWORD}>{i18next.t('Login:forgotPassword')}</a>
       </div>
     </form>
   );
