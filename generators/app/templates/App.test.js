0f2f7b78dad58873ae8165686ae846ff5190abd4
damfinkel <damfinkel@gmail.com>
2019-12-30 17:58:39 -0300

    added snapshot testing in App.test.js

--- generators/app/templates/App.test.js
+++ generators/app/templates/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import App from './App';
 
@@ -17,6 +18,10 @@ describe('#App', () => {
   describe('when mounting the application', () => {
     it('shows the Learn React text', () => {
       expect(component.find('a').props().children).toMatch(/Learn React/);
+    });
+
+    it('matches last snapshot', () => {
+      expect(renderer.create(component).toJSON()).toMatchSnapshot();
     });
   });
 });
