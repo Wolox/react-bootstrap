fd9fb0f7bbd5c81f4f4008147d073bc7b7de7290
Lucas Nestrojil <lucasnestrojil@gmail.com>
2020-01-30 14:12:45 -0300

    refactor checkbox component with hooks & typescript

---
+++ generators/app/templates/src/app/components/Checkbox/index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+interface Props {
+  checked?: boolean;
+  className?: string;
+  inputClassName?: string;
+  labelClassName?: string;
+  label?: string | undefined;
+  name?: string;
+  disabled?: boolean;
+  required?: boolean;
+  onChange?: (event: React.FormEvent<Element>) => void;
+}
+
+function CheckboxContainer({
+  checked = false,
+  className = '',
+  inputClassName = '',
+  labelClassName = '',
+  label = '',
+  name = '',
+  disabled = false,
+  required = false,
+  onChange
+}: Props) {
+  const [isChecked, setIsChecked] = useState(checked);
+
+  const handleToggle = (event: React.FormEvent<Element>) => {
+    setIsChecked(!isChecked);
+    if (onChange) {
+      onChange(event);
+    }
+  };
+
+  return (
+    <div className={className}>
+      <input
+        className={inputClassName}
+        type="checkbox"
+        id={label}
+        name={name}
+        value={label}
+        checked={isChecked}
+        onChange={handleToggle}
+        disabled={disabled}
+        required={required}
+      />
+      {label && (
+        <label className={labelClassName} htmlFor={label}>
+          {label}
+        </label>
+      )}
+    </div>
+  );
+}
+
+export default CheckboxContainer;
