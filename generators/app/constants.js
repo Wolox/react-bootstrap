43696712d61fcd3e0429d73ef9182f17ff196a8b
tomiir <rocchitomas@gmail.com>
2020-02-04 13:32:48 -0300

    Updated generated prettier version to 1.19.1

--- generators/app/constants.js
+++ generators/app/constants.js
@@ -84,7 +84,10 @@ module.exports.FILES_TO_DESTINATION = [LINTER_IGNORE_PATH, RESCRIPTS_PATH, NPMRC
 
 module.exports.CI_CONFIG_FILE = `${CI_PATH}/config.yml`;
 
-module.exports.BOOTSTRAP_TYPES = [{ name: 'Clean', value: false }, { name: 'Customized', value: true }];
+module.exports.BOOTSTRAP_TYPES = [
+  { name: 'Clean', value: false },
+  { name: 'Customized', value: true }
+];
 
 module.exports.DEV_DEPENDENCIES = [
   'eslint-plugin-import@2.18.2',
@@ -100,7 +103,7 @@ module.exports.DEV_DEPENDENCIES = [
   'stylelint-scss@3.5.4',
   'stylelint@9.10.1',
   'husky@^4.0.3',
-  'prettier@1.18.2',
+  'prettier@1.19.1',
   'prettier-eslint@8.8.2',
   'prettier-stylelint@0.4.2',
   'prettier-eslint-cli@5.0.0',
