307e6b5603300641ad5e313696f8517c84a3c28b
Lucas Nestrojil <lucasnestrojil@gmail.com>
2020-02-10 17:05:57 -0300

    Added constant colors and added error label on checkbox

--- generators/app/templates/src/app/components/Checkbox/index.tsx
+++ generators/app/templates/src/app/components/Checkbox/index.tsx
@@ -4,6 +4,8 @@ interface Props {
   checked: boolean;
   className?: string;
   disabled?: boolean;
+  error?: string;
+  errorClassName?: string;
   inputClassName?: string;
   label?: string;
   labelClassName?: string;
@@ -16,6 +18,8 @@ function Checkbox({
   checked,
   className = '',
   disabled = false,
+  error = '',
+  errorClassName = '',
   inputClassName = '',
   label = '',
   labelClassName = '',
@@ -24,23 +28,26 @@ function Checkbox({
   required = false
 }: Props) {
   return (
-    <div className={className}>
-      <input
-        className={inputClassName}
-        type="checkbox"
-        id={label}
-        name={name}
-        value={label}
-        checked={checked}
-        onChange={onChange}
-        disabled={disabled}
-        required={required}
-      />
-      {label && (
-        <label className={labelClassName} htmlFor={label}>
-          {label}
-        </label>
-      )}
+    <div className={`column start ${className}`}>
+      <div>
+        <input
+          className={inputClassName}
+          type="checkbox"
+          id={label}
+          name={name}
+          value={label}
+          checked={checked}
+          onChange={onChange}
+          disabled={disabled}
+          required={required}
+        />
+        {label && (
+          <label className={labelClassName} htmlFor={label}>
+            {label}
+          </label>
+        )}
+      </div>
+      {error && <label className={`text-error ${errorClassName}`}>{error}</label>}
     </div>
   );
 }
