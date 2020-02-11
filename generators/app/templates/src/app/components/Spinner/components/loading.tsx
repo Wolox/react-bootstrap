472a8711e4ad6a20e51c81a7faf24d3aadb33f18
Brian Gerez <bgerez@MacBook-Pro-de-wolox.local>
2020-02-11 18:10:58 -0300

    the SpinnerProps from react-spinkit added

--- generators/app/templates/src/app/components/Spinner/components/loading.tsx
+++ generators/app/templates/src/app/components/Spinner/components/loading.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import Spinner from 'react-spinkit';
+import Spinner, { SpinnerProps } from 'react-spinkit';
 
-import { COLOR_SPINNER, SPINNER_DEFAULT, TYPE_SPINNER } from './constants';
+import { COLOR_SPINNER, SPINNER_DEFAULT } from './constants';
 
-interface Spinner {
-  className?: string;
-  type?: typeof TYPE_SPINNER[number];
-  color?: string;
-}
-
-function Loading({ className = '', type = SPINNER_DEFAULT, color = COLOR_SPINNER }: Spinner) {
-  return <Spinner name={type} fadeIn="half" color={color} className={className} />;
+function Loading({ className = '', name = SPINNER_DEFAULT, color = COLOR_SPINNER, ...rest }: SpinnerProps) {
+  return <Spinner className={className} name={name} color={color} fadeIn="half" {...rest}   />;
 }
 
 export default Loading;
--- generators/app/templates/src/app/components/Spinner/index.tsx
+++ generators/app/templates/src/app/components/Spinner/index.tsx
@@ -1,33 +1,30 @@
 import React from 'react';
+import { SpinnerProps } from 'react-spinkit';
 
 import Loading from './components/loading';
-import { TYPE_SPINNER } from './components/constants';
 import { getDisplayName } from './utils';
 
-interface SpinnerProps {
+interface WithSpinnerProps {
   loading: boolean;
 }
 
-interface CustomSpinner {
+interface CustomSpinner extends SpinnerProps {
   classNameContainer?: string;
-  classNameLoading?: string;
-  colorSpinner?: string;
-  typeLoading?: typeof TYPE_SPINNER[number];
 }
 
 export function withSpinner<P>(
   WrappedComponent: React.ComponentType<P>,
   customSpinner: CustomSpinner = {}
-): React.FC<SpinnerProps & P> {
-  const { classNameContainer = '', classNameLoading, typeLoading, colorSpinner } = customSpinner;
-  function Spinner({ loading, ...passThroughProps }: SpinnerProps) {
+): React.FC<WithSpinnerProps & P> {
+  const { classNameContainer = '', ...rest } = customSpinner;
+  function Spinner({ loading, ...passThroughProps }: WithSpinnerProps) {
     return loading ? (
       <div className={classNameContainer}>
-        <Loading className={classNameLoading} type={typeLoading} color={colorSpinner} />
+        <Loading {...rest} />
       </div>
     ) : (
-      <WrappedComponent {...passThroughProps as P} />
-    );
+        <WrappedComponent {...passThroughProps as P} />
+      );
   }
   Spinner.displayName = `Spinner(${getDisplayName(WrappedComponent)})`;
   return Spinner;
