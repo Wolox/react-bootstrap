74c1db2be486a316de22c6393ad70a74e5df4fac
alejobh <alejo0395@gmail.com>
2020-04-21 11:52:37 -0500

    Added import of fs in deploy.js generator script

--- generators/app/templates/scripts/deploy.js
+++ generators/app/templates/scripts/deploy.js
@@ -1,9 +1,11 @@
-const { exec } = require('child_process');
-const { spawn, spawnSync } = require('child_process');
+const fs = require("fs");
 
-const argv = require('minimist')(process.argv.slice(2));
+const { exec } = require("child_process");
+const { spawn, spawnSync } = require("child_process");
 
-const { error, success, validateEnvs } = require('./utils');
+const argv = require("minimist")(process.argv.slice(2));
+
+const { error, success, validateEnvs } = require("./utils");
 
 const env = argv._[0];
 
@@ -11,12 +13,12 @@ const gzip = argv._[1];
 
 validateEnvs(env);
 
-if (!fs.existsSync('aws.js')) {
+if (!fs.existsSync("aws.js")) {
   error("aws.js file doesn't exist in the root directory");
   process.exit(1);
 }
 
-exec('git rev-parse --abbrev-ref HEAD', (e, stdout, stderr) => {
+exec("git rev-parse --abbrev-ref HEAD", (e, stdout, stderr) => {
   if (stderr || e) {
     error(`Error getting current branch: ${stderr || e}`);
     process.exit(1);
@@ -28,25 +30,31 @@ exec('git rev-parse --abbrev-ref HEAD', (e, stdout, stderr) => {
     error(`Environment ${env} does not match current branch ${currentBranch}`);
     process.exit(1);
   }
-  const getDeployParams = deployParams => {
+  const getDeployParams = (deployParams) => {
     if (gzip) {
-      return [...deployParams, '-g', 'gzip'];
+      return [...deployParams, "-g", "gzip"];
     }
     return deployParams;
   };
 
-  const build = spawn('npm run build', [env], { stdio: 'inherit', shell: true });
+  const build = spawn("npm run build", [env], {
+    stdio: "inherit",
+    shell: true,
+  });
 
-  build.on('close', code => {
+  build.on("close", (code) => {
     if (code !== 0) {
       error(`Failed to build with code ${code}`);
       process.exit(1);
     }
-    const deployParams = ['--env', env];
+    const deployParams = ["--env", env];
     if (gzip) {
-      spawnSync('npm run gzip', { stdio: 'inherit', shell: true });
+      spawnSync("npm run gzip", { stdio: "inherit", shell: true });
     }
     success(`Build successful, deploying to environment '${env}'`);
-    spawn('aws-deploy', getDeployParams(deployParams), { stdio: 'inherit', shell: true });
+    spawn("aws-deploy", getDeployParams(deployParams), {
+      stdio: "inherit",
+      shell: true,
+    });
   });
 });
