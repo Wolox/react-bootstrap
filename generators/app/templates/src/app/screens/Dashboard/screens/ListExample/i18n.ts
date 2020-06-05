549663c9442df9561f7cecf3157f6e19aa06fbd7
Pablo Ferro <pablo.pyp@gmail.com>
2020-06-05 12:57:10 -0300

    Update example title

--- generators/app/templates/src/app/screens/Dashboard/screens/ListExample/i18n.ts
+++ generators/app/templates/src/app/screens/Dashboard/screens/ListExample/i18n.ts
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 
 i18next.addResources('es', 'ListExample', {
-  title: 'Ejemplo de listado',
+  title: 'Ejemplo de listado paginado',
   loading: 'Cargando...',
   error: 'Error'
 });
