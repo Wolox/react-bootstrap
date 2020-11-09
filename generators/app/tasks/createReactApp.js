0c7f4ef403421d24328068d0d4ed82fac0cdaa61
Pablo Ferro <pablo.pyp@gmail.com>
2020-11-09 09:32:51 -0300

    Use react-scripts 4 and typescript template

--- generators/app/tasks/createReactApp.js
+++ generators/app/tasks/createReactApp.js
@@ -2,7 +2,7 @@ const runCommand = require('./runCommand');
 
 module.exports.installCRA = function installCRA() {
   return runCommand({
-    command: ['npm', ['install', '--global', 'create-react-app@^3.0.0']],
+    command: ['npm', ['install', '--global', 'create-react-app']],
     loadingMessage: 'Installing create-react-app',
     successMessage: 'create-react-app installed successfully',
     failureMessage: 'create-react-app installation failed',
@@ -12,7 +12,10 @@ module.exports.installCRA = function installCRA() {
 
 module.exports.runCRA = function runCRA() {
   return runCommand({
-    command: ['create-react-app', [this.projectName, '--scripts-version=react-scripts@^3.0.0']],
+    command: [
+      'create-react-app',
+      [this.projectName, '--scripts-version=react-scripts@^4.0.0', '--template', 'typescript']
+    ],
     loadingMessage: 'Creating app with create-react-app',
     successMessage: 'Created app with create-react-app successfully',
     failureMessage: 'App creation with create-react-app failed',
