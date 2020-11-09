75a6226524e73df867f7c2742b257cbbb6245cb4
Pablo Ferro <pablo.pyp@gmail.com>
2020-11-09 09:43:12 -0300

    Comment customizable steps

--- generators/app/steps/CustomizableSteps/index.js
+++ generators/app/steps/CustomizableSteps/index.js
@@ -1,17 +1,19 @@
-const installDependencies = require('../../tasks/installDependencies');
+// const installDependencies = require('../../tasks/installDependencies');
 
-const copyAllFiles = require('./copyFiles');
-const { DEPENDENCIES, DEV_DEPENDENCIES } = require('./constants');
+// const copyAllFiles = require('./copyFiles');
+// const { DEPENDENCIES, DEV_DEPENDENCIES } = require('./constants');
 
 const CustomizableGenerator = {
   prompting() {
     return Promise.resolve();
   },
-  async configuring() {
-    await installDependencies.bind(this)({ dependencies: DEPENDENCIES, devDependencies: DEV_DEPENDENCIES });
+  configuring() {
+    return Promise.resolve();
+    // await installDependencies.bind(this)({ dependencies: DEPENDENCIES, devDependencies: DEV_DEPENDENCIES });
   },
   writing() {
-    return Promise.resolve().then(copyAllFiles.bind(this));
+    return Promise.resolve();
+    // return Promise.resolve().then(copyAllFiles.bind(this));
   }
 };
 
