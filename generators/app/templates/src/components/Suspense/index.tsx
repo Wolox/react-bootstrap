dd18415672531c4307d7c82506a0e82b34aa6591
Pablo Ferro <pablo.pyp@gmail.com>
2021-03-02 13:42:55 -0300

    Fix suspense

--- generators/app/templates/src/components/Suspense/index.tsx
+++ generators/app/templates/src/components/Suspense/index.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense as ReactSuspense } from 'react';
 
 interface Props {
-  fallback: React.ElementType;
+  fallback?: React.ElementType;
   children: React.ReactNode;
 }
 
-function Suspense({ fallback: Fallback, children }: Props) {
+// eslint-disable-next-line react/no-multi-comp
+function Suspense({ fallback: Fallback = () => <div>Loading...</div>, children }: Props) {
   return <ReactSuspense fallback={<Fallback />}>{children}</ReactSuspense>;
 }
 
