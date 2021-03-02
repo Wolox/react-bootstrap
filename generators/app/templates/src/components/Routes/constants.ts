5b16a553cdae34b35d90a216e6181ee349c18180
Pablo Ferro <pablo.pyp@gmail.com>
2021-03-02 13:24:12 -0300

    Fix new line

--- generators/app/templates/src/components/Routes/constants.ts
+++ generators/app/templates/src/components/Routes/constants.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { lazy } from 'react';
+
 import { Nullable } from 'utils/types';
 import { User } from 'contexts/UserContext/reducer';
 
