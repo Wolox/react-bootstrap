61ddb9072dbe7fcf5a97fea322d248c8b7be2441
alejobh <alejo0395@gmail.com>
2021-05-11 22:17:07 -0500

    typo fix

--- generators/app/templates/src/screens/Dashboard/screens/Home/context/reducer.test.ts
+++ generators/app/templates/src/screens/Dashboard/screens/Home/context/reducer.test.ts
@@ -15,7 +15,7 @@ test('setTech action sets the new tech when there is a tech', () => {
   expect(newState).toEqual({ tech: newTech });
 });
 
-test('resettech action removes the tech', () => {
+test('resetTech action removes the tech', () => {
   const initialState = { tech: 'Svelte' };
   const newState = reducer(initialState, actionCreators.resetTech());
   expect(newState).toEqual({ tech: '' });
