01eed70219cdfeadcc5dc9fe9f897b35697f847b
alejobh <alejo0395@gmail.com>
2021-05-21 16:45:10 -0500

    Removed lang from setupTests

--- generators/app/templates/setupTests.ts
+++ generators/app/templates/setupTests.ts
@@ -1,23 +1,20 @@
 import '@testing-library/jest-dom';
 import 'mutationobserver-shim';
 
-jest.mock('react-i18next', () => {
-  let lang = 'es';
-  return {
-    initReactI18next: {
-      type: '3rdParty',
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      init: () => {}
-    },
-    useTranslation: () => ({
-      t: (key: string, params: Record<string, string>) => (params ? `${key} ${JSON.stringify(params)}` : key),
-      i18n: {
-        changeLanguage: (newLang: string) => (lang = newLang),
-        language: lang
-      }
-    })
-  };
-});
+jest.mock('react-i18next', () => ({
+  initReactI18next: {
+    type: '3rdParty',
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    init: () => {}
+  },
+  useTranslation: () => ({
+    t: (key: string, params: Record<string, string>) => (params ? `${key} ${JSON.stringify(params)}` : key),
+    i18n: {
+      changeLanguage: jest.fn(),
+      language: 'es'
+    }
+  })
+}));
 
 jest.mock('i18next', () => ({
   addResources: jest.fn(),
