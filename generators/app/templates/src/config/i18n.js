11eb64a71ec40de9db1036ac8eac810f0b0224ee
alejobh <alejo0395@gmail.com>
2021-05-21 16:42:04 -0500

    Fix on i18n config and collectCoverageFrom to ignore i18n files

--- generators/app/templates/src/config/i18n.js
+++ generators/app/templates/src/config/i18n.js
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-unction requireAll(requireContext) {
+function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
 }
 
